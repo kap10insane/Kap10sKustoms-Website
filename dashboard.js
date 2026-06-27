2d8a2aa0d233e5c525d59bacf794275b31ec1c9d
kap10insane <geraldhopp@gmail.com>
2026-06-27 15:12:43 -0400

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -525,7 +525,7 @@ const cancelCategoryBtn = document.getElementById("cancelCategoryBtn");
 const closeCategoryModalBtn = document.getElementById("closeCategoryModalBtn");
 
 function openCategoryModal() {
-  console.log("New Category clicked", categoryModal);
+  
 
   if (!categoryModal) return;
   categoryModal.classList.remove("hidden");
